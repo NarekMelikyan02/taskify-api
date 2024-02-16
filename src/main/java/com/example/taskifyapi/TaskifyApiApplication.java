59b0d31eddb4342b5d1b540dfe19780ade41b618
NarekMelikyan02 <nmelikyan773@gmail.com>
2024-02-16 17:13:37 +0400

    master

---
+++ src/main/java/com/example/taskifyapi/TaskifyApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskifyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskifyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskifyApiApplication.class, args);
+    }
+
+}
