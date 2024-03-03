54d4ff9e5feb6ee6fc6f9aefe5585527956f4220
NarekMelikyan02 <nmelikyan773@gmail.com>
2024-03-03 21:50:06 +0400

    Configured admin controller

---
+++ src/main/java/com/example/taskifyapi/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.example.taskifyapi.controller;
+
+import com.example.taskifyapi.entity.UserEntity;
+import com.example.taskifyapi.model.AuthenticationResponse;
+import com.example.taskifyapi.service.UserService;
+import java.util.UUID;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("Users")
+public class UserController {
+
+  private final UserService userService;
+
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @PostMapping("/register")
+  public ResponseEntity<AuthenticationResponse> register(@RequestBody UserEntity request) {
+    return ResponseEntity.ok(userService.register(request));
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<AuthenticationResponse> login(@RequestBody UserEntity request) {
+    return ResponseEntity.ok(userService.authenticate(request));
+  }
+
+  @PutMapping("updateBy/{id}")
+  ResponseEntity<Void> updateById(@PathVariable UUID id, @RequestBody UserEntity request) {
+    userService.updateUser(request, id);
+    return new ResponseEntity<>(HttpStatusCode.valueOf(200));
+  }
+}
---
+++ src/main/java/com/example/taskifyapi/service/UserService.java
@@ -0,0 +1,62 @@
+package com.example.taskifyapi.service;
+
+import com.example.taskifyapi.entity.UserEntity;
+import com.example.taskifyapi.exeptions.UserNotFoundException;
+import com.example.taskifyapi.model.AuthenticationResponse;
+import com.example.taskifyapi.repository.UserRepository;
+import com.example.taskifyapi.securityconfig.JwtService.JwtService;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class UserService {
+
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+  private final JwtService jwtService;
+  private final AuthenticationManager authenticationManager;
+
+  public AuthenticationResponse register(UserEntity request) {
+    UserEntity user = new UserEntity();
+    user.setFirstName(request.getFirstName());
+    user.setLastName(request.getLastName());
+    user.setGender(request.getGender());
+    user.setEmail(request.getEmail());
+    user.setPassword(passwordEncoder.encode(request.getPassword()));
+    user.setRole(request.getRole());
+    user.setCreated(LocalDateTime.now());
+    userRepository.save(user);
+
+    String token = jwtService.generateToken(user);
+    return new AuthenticationResponse(token);
+  }
+
+  public AuthenticationResponse authenticate(UserEntity request) {
+    authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+    UserEntity user = userRepository.findByEmail(request.getUsername()).orElseThrow();
+    String token = jwtService.generateToken(user);
+    return new AuthenticationResponse(token);
+  }
+
+  public void updateUser(final UserEntity request, final UUID id) {
+    UserEntity user =
+        userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User not found"));
+    user.setFirstName(request.getFirstName());
+    user.setLastName(request.getLastName());
+    user.setGender(request.getGender());
+    user.setEmail(request.getEmail());
+    user.setPassword(request.getPassword());
+    user.setUpdated(LocalDateTime.now());
+    log.info("Successfully updated user by id: {}", request.getId());
+    userRepository.save(user);
+  }
+}
